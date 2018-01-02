5d8cd6e17456102c366d3a16d127c48e507364b2
Carlos David Nexans <carlmycol@gmail.com>
2018-01-01 23:30:13 -0300

    Inicializa npm e instala node-fetch

--- vistazo-avanzado/fetch.js
+++ vistazo-avanzado/fetch.js
@@ -6,6 +6,6 @@ const fetch = require('node-fetch');
 const posts = fetch('https://jsonplaceholder.typicode.com/posts');
 console.log('Cargando posts');
 
-posts.then(
-  datos => console.log(datos)
-);
+posts
+  .then(respuesta => respuesta.json())
+  .then(datos => console.log(datos[0]));
